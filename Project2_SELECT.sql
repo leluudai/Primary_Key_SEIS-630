1aca2c78ae6e14c20838e73ebe7982f5dace789a
Guled <ahme4146@stthomas.edu>
2020-11-08 13:23:26 -0600

    Added select sql

---
+++ Project2_SELECT.sql
@@ -0,0 +1,3 @@
+/* Part C of Exhibit B (Ask professor about looking up contract associated with project) */
+SELECT PROJECTNUMBER, PROJECTADDRESS, PROJECTDESCRIPTION FROM PROJECT Where PROJECTNUMBER = 'IL-SJB-335-005';
+
