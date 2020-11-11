04dd5053e2e7dabe24eab4ccf0ef32b1f0595280
Dai Le <dai.le@spok.com>
2020-11-10 19:22:41 -0600

    Adding Insert Data for EXHIBIT C - Part C -Detailed Pay Scale Breakdown for Regular Hours (Overtime =1.5 X the Basic Rate)

--- Project2_SELECT.sql
+++ Project2_SELECT.sql
@@ -1,3 +1,7 @@
 /* Part C of Exhibit B (Ask professor about looking up contract associated with project) */
 SELECT PROJECTNUMBER, PROJECTADDRESS, PROJECTDESCRIPTION FROM PROJECT Where PROJECTNUMBER = 'IL-SJB-335-005';
 
+
+-- SELECT QUERY Data for EXHIBIT C - Part C -Detailed Pay Scale Breakdown for Regular Hours (Overtime =1.5 X the Basic Rate)
+SELECT SKILLCODE, SKILLCLASSIFICATION, BASICHOURLYRATE, FRINGE, BASICHOURLYRATE + FRINGE
+    AS Total_Compensation FROM SKILL ORDER BY SKILLID;
